4950024e6aa8485526baab28840ecd2bceb4a66f
riotamoriya <riotamoriya@gmail.com>
2025-01-06 15:53:37 +0900

    react バージョン変更

--- src/@types/crypto-js.d.ts
+++ src/@types/crypto-js.d.ts
@@ -1,1 +1,10 @@
- 
+declare module 'crypto-js' {
+  export const SHA256: (message: string) => string;
+  export const enc: {
+    Utf8: {
+      stringify: (wordArray: any) => string;
+      parse: (str: string) => any;
+    };
+  };
+  // Add other exports as needed
+} 
