a1ccdc12f2e6229680f852b5aedb27736a07e11b
riotamoriya <riotamoriya@gmail.com>
2025-01-06 16:08:20 +0900

    修正

--- src/@types/crypto-js.d.ts
+++ src/@types/crypto-js.d.ts
@@ -1,10 +1,14 @@
+// src/@types/crypto-js.d.ts を更新
 declare module 'crypto-js' {
-  export const SHA256: (message: string) => string;
-  export const enc: {
-    Utf8: {
-      stringify: (wordArray: any) => string;
-      parse: (str: string) => any;
-    };
-  };
-  // Add other exports as needed
-} 
+  interface WordArray {
+    words: number[];
+    sigBytes: number;
+    toString(encoder?: unknown): string;
+  }
+  
+  interface HashStatic {
+    (message: string | WordArray): WordArray;
+  }
+
+  export const SHA256: HashStatic;
+}
